e0ec18e12898e1a8d565c7871d20fb282b8f8768
Nicholas Alexeev <nickalexeev@protonmail.com>
2020-09-23 15:53:46 -0800

    working on prototype system

---
+++ prototype_render_api/src/lib.rs
@@ -0,0 +1,46 @@
+enum Command{
+    Text{
+        pos: (f32,f32),
+        text: String,
+    },
+    Line{
+        start: (f32,f32),
+        end: (f32,f32),
+    }
+}
+struct RenderApi{
+
+}
+impl RenderApi{
+    pub fn draw_boarder(&self,dims:WindowDimensions){}
+    pub fn draw_text(&self,cords: (f32,f32),text:String){}
+    pub fn draw_line(&self,start: (f32,f32),end: (f32,f32)){}
+}
+struct WindowDimensions{
+
+}
+trait Terminal{
+    fn draw(&self,render_api:&mut RenderApi){
+        self.getCommands().iter().map(|c|
+        match c{
+            Command::Text{
+                pos,
+                text
+            }=>render_api.draw_text(pos.clone(), text.clone()),
+            Command::Line{
+                start,
+                end
+            }=>render_api.draw_line(start.clone(), end.clone()),
+            
+        }
+        ).for_each(drop);
+    }
+    fn getCommands(&self)->Vec<Command>;
+}
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
