384168bdd6cbc527ef7d115043ebaef216de6dea
Nicholas Alexeev <nickalexeev@protonmail.com>
2020-09-27 16:38:23 -0800

    got ctor working

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,22 +1,3 @@
-#[cfg(feature = "dx11")]
-extern crate gfx_backend_dx11 as back;
-#[cfg(feature = "dx12")]
-extern crate gfx_backend_dx12 as back;
-#[cfg(not(any(
-    feature = "vulkan",
-    feature = "dx11",
-    feature = "dx12",
-    feature = "metal",
-    feature = "gl",
-)))]
-extern crate gfx_backend_empty as back;
-#[cfg(all(unix, feature = "gl"))]
-extern crate gfx_backend_gl as back;
-#[cfg(feature = "metal")]
-extern crate gfx_backend_metal as back;
-#[cfg(feature = "vulkan")]
-extern crate gfx_backend_vulkan as back;
-
 #[cfg(target_arch = "wasm32")]
 use wasm_bindgen::prelude::*;
 
@@ -29,121 +10,11 @@ pub fn wasm_main() {
 mod front_end_api;
 mod renderer;
 mod text_render;
+use front_end_api::build_vulkan_context;
 use gfx_hal::{prelude::*, window};
 use renderer::Renderer;
 pub use renderer::Updater;
 
 fn main() {
-    let mut text_render = text_render::TextRender::new(renderer::DIMS.width, renderer::DIMS.height);
-    #[cfg(target_arch = "wasm32")]
-    console_log::init_with_level(log::Level::Debug).unwrap();
-
-    #[cfg(not(target_arch = "wasm32"))]
-    env_logger::init();
-
-    #[cfg(not(any(
-        feature = "vulkan",
-        feature = "dx11",
-        feature = "dx12",
-        feature = "metal",
-        feature = "gl",
-    )))]
-    eprintln!(
-        "You are running the example with the empty backend, no graphical output is to be expected"
-    );
-
-    let event_loop = winit::event_loop::EventLoop::new();
-
-    let wb = winit::window::WindowBuilder::new()
-        .with_min_inner_size(winit::dpi::Size::Logical(winit::dpi::LogicalSize::new(
-            64.0, 64.0,
-        )))
-        .with_inner_size(winit::dpi::Size::Physical(winit::dpi::PhysicalSize::new(
-            renderer::DIMS.width,
-            renderer::DIMS.height,
-        )))
-        .with_title("quad".to_string());
-
-    // instantiate backend
-    #[cfg(not(target_arch = "wasm32"))]
-    let (_window, instance, mut adapters, surface) = {
-        let window = wb.build(&event_loop).unwrap();
-        let instance =
-            back::Instance::create("gfx-rs quad", 1).expect("Failed to create an instance!");
-        let adapters = instance.enumerate_adapters();
-        let surface = unsafe {
-            instance
-                .create_surface(&window)
-                .expect("Failed to create a surface!")
-        };
-        // Return `window` so it is not dropped: dropping it invalidates `surface`.
-        (window, Some(instance), adapters, surface)
-    };
-
-    #[cfg(target_arch = "wasm32")]
-    let (_window, instance, mut adapters, surface) = {
-        let (window, surface) = {
-            let window = wb.build(&event_loop).unwrap();
-            web_sys::window()
-                .unwrap()
-                .document()
-                .unwrap()
-                .body()
-                .unwrap()
-                .append_child(&winit::platform::web::WindowExtWebSys::canvas(&window))
-                .unwrap();
-            let surface = back::Surface::from_raw_handle(&window);
-            (window, surface)
-        };
-
-        let adapters = surface.enumerate_adapters();
-        (window, None, adapters, surface)
-    };
-
-    for adapter in &adapters {
-        println!("{:?}", adapter.info);
-    }
-
-    let adapter = adapters.remove(0);
-
-    let mut renderer = Renderer::new(instance, surface, adapter);
-
-    renderer.render(&mut text_render);
-
-    // It is important that the closure move captures the Renderer,
-    // otherwise it will not be dropped when the event loop exits.
-    event_loop.run(move |event, _, control_flow| {
-        *control_flow = winit::event_loop::ControlFlow::Wait;
-
-        match event {
-            winit::event::Event::WindowEvent { event, .. } => match event {
-                winit::event::WindowEvent::CloseRequested => {
-                    *control_flow = winit::event_loop::ControlFlow::Exit
-                }
-                winit::event::WindowEvent::KeyboardInput {
-                    input:
-                        winit::event::KeyboardInput {
-                            virtual_keycode, ..
-                        },
-                    ..
-                } => {
-                    text_render.update(virtual_keycode);
-                }
-
-                winit::event::WindowEvent::Resized(dims) => {
-                    println!("resized to {:?}", dims);
-                    renderer.dimensions = window::Extent2D {
-                        width: dims.width,
-                        height: dims.height,
-                    };
-                    renderer.recreate_swapchain();
-                }
-                _ => {}
-            },
-            winit::event::Event::RedrawEventsCleared => {
-                renderer.render(&mut text_render);
-            }
-            _ => {}
-        }
-    });
+    build_vulkan_context();
 }
